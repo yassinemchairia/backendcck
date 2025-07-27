307b00aeb1ca7397c49a5ba3f6ef26382800ebc1
yassinemchairia <115500157+yassinemchairia@users.noreply.github.com>
2025-07-27 12:21:54 +0100

    Update SecurityConfig.java

--- src/main/java/com/example/cckback/security/SecurityConfig.java
+++ src/main/java/com/example/cckback/security/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:4200",  // Angular
+                "http://192.168.107.129:4200",  // Angular
                 "http://localhost:5000"   // Flask
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
