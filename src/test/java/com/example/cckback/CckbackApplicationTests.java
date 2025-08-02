dd1c419887e0397060ff8cdfde554c6ec8ed7c22
yassinemchairia <115500157+yassinemchairia@users.noreply.github.com>
2025-08-02 13:03:23 +0100

    Update CckbackApplicationTests.java

--- src/test/java/com/example/cckback/CckbackApplicationTests.java
+++ src/test/java/com/example/cckback/CckbackApplicationTests.java
@@ -1,8 +1,12 @@
 package com.example.cckback;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest
 class CckbackApplicationTests {
 
