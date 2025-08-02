a41117d18f377b8d8f552987af455deff8e819bd
yassinemchairia <115500157+yassinemchairia@users.noreply.github.com>
2025-08-02 13:27:47 +0100

    Update CckbackApplicationTests.java

--- src/test/java/com/example/cckback/CckbackApplicationTests.java
+++ src/test/java/com/example/cckback/CckbackApplicationTests.java
@@ -1,15 +1,19 @@
 package com.example.cckback;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class CckbackApplicationTests {
 
+    @MockBean
+    private SurveillanceService surveillanceService; // Mock le service
+
     @Test
     void contextLoads() {
     }
-
 }
