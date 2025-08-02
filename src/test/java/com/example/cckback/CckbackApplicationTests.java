e7494c748f1d82b0702e2ccb03bd7b99351f807a
yassinemchairia <115500157+yassinemchairia@users.noreply.github.com>
2025-08-02 13:33:28 +0100

    Update CckbackApplicationTests.java

--- src/test/java/com/example/cckback/CckbackApplicationTests.java
+++ src/test/java/com/example/cckback/CckbackApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.cckback;
 
+import com.example.cckback.service.SurveillanceService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
